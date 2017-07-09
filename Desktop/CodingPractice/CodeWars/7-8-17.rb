1328e69c271ef7595151296c5e697a9dd60289a8
NicoleDVH <nicole12909@gmail.com>
2017-07-08 22:33:48 -0400

    solved another

--- Desktop/CodingPractice/CodeWars/7-8-17.rb
+++ Desktop/CodingPractice/CodeWars/7-8-17.rb
@@ -12,3 +12,13 @@ def break_chocolate(n, m)
     return breaks
   end
 end
+
+#A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has two digits, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+
+def digital_root(n)
+  while n > 10
+    x = n.to_s.split(//).map {|i| i.to_i}
+    n = x.reduce(:+)
+  end
+  return n
+end
